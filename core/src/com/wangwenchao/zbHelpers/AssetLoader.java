3f5490dd9a62cf3c1d1e07c2d508699f887dafc8
Wenchao <wangwenchao@gmail.com>
2014-12-01 16:16:52 -0500

    add button

--- core/src/com/wangwenchao/zbHelpers/AssetLoader.java
+++ core/src/com/wangwenchao/zbHelpers/AssetLoader.java
@@ -11,13 +11,11 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class AssetLoader {
 	
-	public static Texture texture;
-	public static TextureRegion bg, grass;
+	public static Texture texture, logoTexture;
+	public static TextureRegion logo, zbLogo, bg, grass, bird, birdDown, birdUp, skullUp, skullDown, bar, playButtonUp, playButtonDown;;
 	
 	public static Animation birdAnimation;
-	public static TextureRegion bird, birdDown, birdUp;
 	
-	public static TextureRegion skullUp, skullDown, bar;
 	public static Sound dead, flap, coin;
 	
 	public static BitmapFont font, shadow;
@@ -26,8 +24,20 @@ public class AssetLoader {
 	
 	public static void load() {
 		
+		logoTexture = new Texture(Gdx.files.internal("data/logo.png"));
+        logoTexture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+        logo = new TextureRegion(logoTexture, 0, 0, 512, 114);
+        
 		texture = new Texture(Gdx.files.internal("data/texture.png"));
 		texture.setFilter(TextureFilter.Nearest, TextureFilter.Nearest);
+		
+		playButtonUp = new TextureRegion(texture, 0, 83, 29, 16);
+        playButtonDown = new TextureRegion(texture, 29, 83, 29, 16);
+        playButtonUp.flip(false, true);
+        playButtonDown.flip(false, true);
+
+        zbLogo = new TextureRegion(texture, 0, 55, 135, 24);
+        zbLogo.flip(false, true);
 		
 		bg = new TextureRegion(texture, 0, 0, 136, 43);
 		bg.flip(false, true);
